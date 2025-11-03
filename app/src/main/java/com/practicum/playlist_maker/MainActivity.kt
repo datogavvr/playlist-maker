05a8826fd817d144cb9e5fb4364858dc9bafe727
Dato <dato.gavrilin@gmail.com>
2025-11-04 02:34:58 +0300

    Добавление стартового экрана

--- app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.practicum.playlist_maker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.practicum.playlist_maker.ui.theme.PlaylistmakerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlaylistmakerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +46,95 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        // Первый маленький бокс сверху
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .background(color = MaterialTheme.colorScheme.primary)
+                    .padding(12.dp),
+                text = "Playlist maker",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+
+        // Второй бокс на всю оставшуюся часть экрана
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f) // Занимает все оставшееся пространство
+                .background(Color.White)
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+            ) {
+                // Кнопка Поиск
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "Нажата кнопка \"Поиск\"", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Поиск")
+                }
+
+                // Кнопка Плейлисты
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "Нажата кнопка \"Плейлисты\"", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Плейлисты")
+                }
+
+                // Кнопка Избранное
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "Нажата кнопка \"Избранное\"", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Избранное")
+                }
+
+                // Кнопка Настройки
+                Button(
+                    onClick = {
+                        Toast.makeText(context, "Нажата кнопка \"Настройки\"", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Настройки")
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "portrait", showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    PlaylistmakerTheme {
-        Greeting("Android")
+fun MainScreenPreview() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        MainScreen(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
     }
 }
