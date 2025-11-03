c9cb9ff7b0be802b4fe8797f14bd86e7026c7af0
Dato <dato.gavrilin@gmail.com>
2025-11-04 02:34:31 +0300

    Выбор цветов

--- app/src/main/java/com/practicum/playlist_maker/ui/theme/Color.kt
+++ app/src/main/java/com/practicum/playlist_maker/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.practicum.playlist_maker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black80 = Color(0xFF1A1B22)
+val Grey80 = Color(0xFF26272A)
+val Pink80 = Color(0xFF38393B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF3772E7)
+val PurpleGrey40 = Color(0xFF2E5CB9)
+val Pink40 = Color(0xFF193772)
