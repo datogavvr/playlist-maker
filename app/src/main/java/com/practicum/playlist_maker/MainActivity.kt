502d7436b504b4496840155f3b107a890b4f8d15
Dato <dato.gavrilin@gmail.com>
2025-11-05 00:03:33 +0300

    Обновил дизайн главного экрана

--- app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.practicum.playlist_maker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,21 +10,35 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.LibraryMusic
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,10 +50,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PlaylistmakerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
                 }
             }
         }
@@ -46,95 +62,144 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun GenericButton(
+    icon: ImageVector,
+    nameButton: String,
+    onClick: () -> Unit,
+    showToast: Boolean = false
+) {
     val context = LocalContext.current
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
-        // Первый маленький бокс сверху
 
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .background(color = MaterialTheme.colorScheme.primary)
-                    .padding(12.dp),
-                text = "Playlist maker",
-                color = Color.White,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
+    Button(
+        onClick = {
+            if (showToast) {
+                Toast.makeText(context, "Нажата кнопка \"$nameButton\"", Toast.LENGTH_SHORT).show()
+            }
+            onClick()
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(66.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = nameButton,
+                modifier = Modifier.size(28.dp)
             )
 
+            Spacer(modifier = Modifier.width(12.dp))
 
-        // Второй бокс на всю оставшуюся часть экрана
+            Text(
+                text = nameButton,
+                textAlign = TextAlign.Start,
+                fontSize = 18.sp,
+                modifier = Modifier.weight(1f)
+            )
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                contentDescription = "Стрелка",
+                modifier = Modifier.size(20.dp),
+                tint = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary)
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(12.dp),
+            text = stringResource(R.string.app_name),
+            color = Color.White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+        )
+
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f) // Занимает все оставшееся пространство
-                .background(Color.White)
+                .weight(1f)
+                .background(
+                    MaterialTheme.colorScheme.background,
+                    shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+                )
         ) {
-
-
             Column(
-                modifier = Modifier
-                    .padding(8.dp)
+                modifier = Modifier.padding(16.dp)
             ) {
-                // Кнопка Поиск
-                Button(
+                GenericButton(
+                    icon = Icons.Filled.Search,
+                    nameButton = stringResource(R.string.search),
                     onClick = {
-                        Toast.makeText(context, "Нажата кнопка \"Поиск\"", Toast.LENGTH_SHORT)
-                            .show()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Поиск")
-                }
+                        val intent = Intent(context, SearchActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                )
 
-                // Кнопка Плейлисты
-                Button(
-                    onClick = {
-                        Toast.makeText(context, "Нажата кнопка \"Плейлисты\"", Toast.LENGTH_SHORT)
-                            .show()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Плейлисты")
-                }
+                Spacer(modifier = Modifier.height(16.dp))
 
-                // Кнопка Избранное
-                Button(
-                    onClick = {
-                        Toast.makeText(context, "Нажата кнопка \"Избранное\"", Toast.LENGTH_SHORT)
-                            .show()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Избранное")
-                }
+                GenericButton(
+                    icon = Icons.Filled.LibraryMusic,
+                    nameButton = stringResource(R.string.playlists),
+                    onClick = {},
+                    showToast = true
+                )
 
-                // Кнопка Настройки
-                Button(
+                Spacer(modifier = Modifier.height(16.dp))
+
+                GenericButton(
+                    icon = Icons.Filled.FavoriteBorder,
+                    nameButton = stringResource(R.string.favorite),
+                    onClick = {},
+                    showToast = true
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                GenericButton(
+                    icon = Icons.Filled.Settings,
+                    nameButton = stringResource(R.string.settings),
                     onClick = {
-                        Toast.makeText(context, "Нажата кнопка \"Настройки\"", Toast.LENGTH_SHORT)
-                            .show()
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Настройки")
-                }
+                        val intent = Intent(context, SettingsActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                )
             }
         }
     }
 }
 
-@Preview(name = "portrait", showSystemUi = true)
+@Preview
 @Composable
 fun MainScreenPreview() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        MainScreen(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-        )
+    PlaylistmakerTheme {
+        MainScreen()
+    }
+}
+
+@Preview
+@Composable
+fun DarkMainScreenPreview() {
+    PlaylistmakerTheme(darkTheme = true) {
+        MainScreen()
     }
 }
