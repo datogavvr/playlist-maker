6ecfd6c718e31a57e9289271daae9ba6b1136306
Dato <dato.gavrilin@gmail.com>
2025-12-05 00:12:33 +0300

    Изменил параметры трека

--- app/src/main/java/com/practicum/playlist_maker/data/network/Track.kt
+++ app/src/main/java/com/practicum/playlist_maker/data/network/Track.kt
@@ -1,7 +1,11 @@
 package com.practicum.playlist_maker.data.network
 
 data class Track(
+    val id: Long,
     val trackName: String,
     val artistName: String,
-    val trackTime: String
+    val trackTime: String,
+    val image: String,
+    var favorite: Boolean,
+    var playlistId: MutableList<Long>
 )
