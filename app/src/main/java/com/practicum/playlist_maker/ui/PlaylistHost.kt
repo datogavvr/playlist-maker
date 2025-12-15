603e1a9419dae2938ffbdb11d259e747df118d94
Dato <dato.gavrilin@gmail.com>
2025-12-16 02:50:20 +0300

    Добавил экран плейлиста

--- app/src/main/java/com/practicum/playlist_maker/ui/PlaylistHost.kt
+++ app/src/main/java/com/practicum/playlist_maker/ui/PlaylistHost.kt
@@ -3,19 +3,25 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.practicum.playlist_maker.creator.Creator
 import com.practicum.playlist_maker.ui.activity.MainScreen
 import com.practicum.playlist_maker.ui.screen.CreatePlaylistScreen
+import com.practicum.playlist_maker.ui.screen.PlaylistScreen
 import com.practicum.playlist_maker.ui.screen.PlaylistsScreen
 import com.practicum.playlist_maker.ui.screen.Screen
 import com.practicum.playlist_maker.ui.viewmodel.SearchViewModel
 import com.practicum.playlist_maker.ui.screen.SearchScreen
 import com.practicum.playlist_maker.ui.screen.SettingsScreen
 import com.practicum.playlist_maker.ui.screen.TrackDetailsScreen
+import com.practicum.playlist_maker.ui.viewmodel.PlaylistViewModel
 import com.practicum.playlist_maker.ui.viewmodel.PlaylistsViewModel
 import com.practicum.playlist_maker.ui.viewmodel.TrackDetailsViewModel
 
@@ -36,7 +42,7 @@ fun PlaylistHost(navController: NavHostController) {
         composable(Screen.MAIN.name) {
             MainScreen(
                 onSearchClick = { navController.navigate(Screen.SEARCH.name) },
-                onPlaylistsClick = {navController.navigate(Screen.PLAYLISTS.name) },
+                onPlaylistsClick = {navController.navigate(Screen.ALL_PLAYLISTS.name) },
                 onSettingsClick = { navController.navigate(Screen.SETTINGS.name) }
             )
         }
@@ -67,15 +73,13 @@ fun PlaylistHost(navController: NavHostController) {
         }
 
         // экран плейлистов
-        composable(Screen.PLAYLISTS.name) {
+        composable(Screen.ALL_PLAYLISTS.name) {
             val playlistsViewModel: PlaylistsViewModel = viewModel()
-            val context = LocalContext.current
             PlaylistsScreen(
                 viewModel = playlistsViewModel,
                 addNewPlaylist = { navController.navigate(Screen.CREATE_PLAYLIST.name) },
                 onPlaylistClick = { playlistId ->
-                    Toast.makeText(context, "Плейлист $playlistId", Toast.LENGTH_SHORT).show()
-//                    navController.navigate("${Screen.PLAYLIST_DETAILS.name}/$playlistId")
+                    navController.navigate("${Screen.PLAYLIST_DETAILS.name}/$playlistId")
                 },
                 onBack = { navController.popBackStack() },
             )
@@ -87,6 +91,44 @@ fun PlaylistHost(navController: NavHostController) {
                 onBack = { navController.popBackStack() }
             )
         }
+
+        // экран плейлиста
+        composable(
+            route = "${Screen.PLAYLIST_DETAILS.name}/{playlistId}",
+            arguments = listOf(navArgument("playlistId") { type = NavType.LongType })
+        ) { backStack ->
+            val playlistId = backStack.arguments?.getLong("playlistId") ?: 0L
+            val context = LocalContext.current
+
+            val playlistsRepository = remember { Creator.getPlaylistsRepository() }
+            val tracksRepository = remember { Creator.getTracksRepository() }
+
+            val playlistViewModel: PlaylistViewModel = viewModel(
+                factory = PlaylistViewModel.Factory(
+                    playlistId,
+                    playlistsRepository,
+                    tracksRepository)
+            )
+
+            PlaylistScreen(
+                playlistViewModel = playlistViewModel,
+                onTrackClick = { trackId ->
+                    navController.navigate("${Screen.TRACK_DETAILS.name}/$trackId")
+                },
+                onEditClick = {
+                    Toast.makeText(context, "Редактирование", Toast.LENGTH_SHORT).show()
+                },
+                onDeleteClick = {
+                    Toast.makeText(context, "Удалено", Toast.LENGTH_SHORT).show()
+                    navController.popBackStack()
+                },
+                onShareClick = {
+                    Toast.makeText(context, "Поделиться", Toast.LENGTH_SHORT).show()
+                },
+                onBack = { navController.popBackStack() }
+            )
+        }
+
 
         // экран избранного
         composable(Screen.FAVORITES.name) {
---
+++ app/src/main/java/com/practicum/playlist_maker/ui/viewmodel/PlaylistViewModel.kt
@@ -0,0 +1,75 @@
+package com.practicum.playlist_maker.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.practicum.playlist_maker.data.network.Playlist
+import com.practicum.playlist_maker.domain.PlaylistsRepository
+import com.practicum.playlist_maker.domain.TracksRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class PlaylistViewModel(
+    private val playlistId: Long,
+    private val playlistsRepository: PlaylistsRepository,
+    private val tracksRepository: TracksRepository
+) : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    val playlist: StateFlow<Playlist?> =
+        playlistsRepository.getPlaylist(playlistId)
+            .onEach { _isLoading.value = false }
+            .stateIn(viewModelScope, SharingStarted.Lazily, null)
+
+    // суммарная длительность в минутах
+    val totalMinutes: StateFlow<Long> = playlist
+        .map { playlist ->
+            playlist?.tracks?.sumOf { it.trackTimeMillis }?.div(1000)?.div(60) ?: 0L
+        }
+        .stateIn(viewModelScope, SharingStarted.Lazily, 0L)
+
+    // количество треков
+    val trackCountText: StateFlow<String> = playlist
+        .map { pl ->
+            val count = pl?.tracks?.size ?: 0
+            "$count ${trackWord(count)}"
+        }
+        .stateIn(viewModelScope, SharingStarted.Lazily, "0 треков")
+
+    // склонения
+    private fun trackWord(count: Int): String {
+        val rem100 = count % 100
+        val rem10 = count % 10
+        return when {
+            rem100 in 11..19 -> "треков"
+            rem10 == 1 -> "трек"
+            rem10 in 2..4 -> "трека"
+            else -> "треков"
+        }
+    }
+
+    fun deleteTrackFromPlaylist(trackId: Long) {
+        viewModelScope.launch {
+            tracksRepository.deleteTrackFromPlaylist(
+                trackId = trackId,
+                playlistId = playlistId
+            )
+        }
+    }
+
+    class Factory(
+        private val playlistId: Long,
+        private val playlistsRepository: PlaylistsRepository,
+        private val tracksRepository: TracksRepository
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return PlaylistViewModel(
+                playlistId,
+                playlistsRepository,
+                tracksRepository
+            ) as T
+        }
+    }
+}
