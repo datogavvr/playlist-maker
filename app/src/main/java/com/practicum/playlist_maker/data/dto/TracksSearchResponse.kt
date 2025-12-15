78cd55efe45b9883430282d9f75b14e8046a7c0d
Dato <dato.gavrilin@gmail.com>
2025-12-16 02:40:08 +0300

    Улучшил дизайн Изменил плейсхолдер для незагруженных треков Улучшил обработку состояний

--- app/src/main/java/com/practicum/playlist_maker/data/dto/TracksSearchResponse.kt
+++ app/src/main/java/com/practicum/playlist_maker/data/dto/TracksSearchResponse.kt
@@ -1,3 +1,6 @@
 package com.practicum.playlist_maker.data.dto
 
-class TracksSearchResponse(val results: List<TrackDto>) : BaseResponse()
+class TracksSearchResponse(
+    val resultCount: Int = 0,
+    val results: List<TrackDto>
+) : BaseResponse()
