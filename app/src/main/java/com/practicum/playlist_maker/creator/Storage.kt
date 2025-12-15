7f51ec087df67932fd3b547960e9e0715974a870
Dato <dato.gavrilin@gmail.com>
2025-12-16 02:58:16 +0300

    Ничего интересного

--- app/src/main/java/com/practicum/playlist_maker/creator/Storage.kt
+++ app/src/main/java/com/practicum/playlist_maker/creator/Storage.kt
@@ -3,105 +3,95 @@ package com.practicum.playlist_maker.creator
 import com.practicum.playlist_maker.data.dto.TrackDto
 
 class Storage {
-    private val listTracks = listOf(
-        TrackDto(
-            id = 1,
-            trackName = "25",
-            artistName = "Markul",
-            trackTime = "2:53",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 2,
-            trackName = "Baptized In Fear",
-            artistName = "The Weeknd",
-            trackTime = "3:52", // 3:52
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 3,
-            trackName = "BOYS DON'T CRY",
-            artistName = "GONE.Fludd",
-            trackTime = "2:27",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 4,
-            trackName = "позову звезды смотреть на тебя",
-            artistName = "мартин",
-            trackTime = "3:21",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 5,
-            trackName = "St. Patrick's",
-            artistName = "Central Cee",
-            trackTime = "2:40",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 6,
-            trackName = "Zima Blue",
-            artistName = "Markul",
-            trackTime = "3:02",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 7,
-            trackName = "Obsessed With You",
-            artistName = "Central Cee",
-            trackTime = "1:48",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 8,
-            trackName = "Ordinary",
-            artistName = "Fabiene Se",
-            trackTime = "2:20",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 9,
-            trackName = "Lucid Dreams",
-            artistName = "Juice WRLD",
-            trackTime = "3:59",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        ),
-        TrackDto(
-            id = 9,
-            trackName = "Ordinary Life",
-            artistName = "The Weeknd",
-            trackTime = "3:41",
-            image = "",
-            favorite = false,
-            playlistId = mutableListOf(0)
-        )
-    )
+//    private val listTracks = listOf(
+//        TrackDto(
+//            id = 1,
+//            trackName = "25",
+//            artistName = "Markul",
+//            trackTimeMillis = 173000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 2,
+//            trackName = "Baptized In Fear",
+//            artistName = "The Weeknd",
+//            trackTimeMillis = 232000, // 3:52
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 3,
+//            trackName = "BOYS DON'T CRY",
+//            artistName = "GONE.Fludd",
+//            trackTimeMillis = 147000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 4,
+//            trackName = "позову звезды смотреть на тебя",
+//            artistName = "мартин",
+//            trackTimeMillis = 201000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 5,
+//            trackName = "St. Patrick's",
+//            artistName = "Central Cee",
+//            trackTimeMillis = 160000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 6,
+//            trackName = "Zima Blue",
+//            artistName = "Markul",
+//            trackTimeMillis = 182000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 7,
+//            trackName = "Obsessed With You",
+//            artistName = "Central Cee",
+//            trackTimeMillis = 108000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 8,
+//            trackName = "Ordinary",
+//            artistName = "Fabiene Se",
+//            trackTimeMillis = 140000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 9,
+//            trackName = "Lucid Dreams",
+//            artistName = "Juice WRLD",
+//            trackTimeMillis = 239000,
+//            previewUrl = null,
+//            image = null
+//        ),
+//        TrackDto(
+//            id = 9,
+//            trackName = "Ordinary Life",
+//            artistName = "The Weeknd",
+//            trackTimeMillis = 221000,
+//            previewUrl = null,
+//            image = null
+//        )
+//    )
 
-    fun search(request: String): List<TrackDto> {
-        if (request.isBlank()) return emptyList()
-        val result = listTracks.filter {
-            it.trackName.lowercase().contains(request.lowercase()) ||
-                    it.artistName.lowercase().contains(request.lowercase())
-        }
-        return result
-    }
+//    fun search(request: String): List<TrackDto> {
+//        if (request.isBlank()) return emptyList()
+//        val result = listTracks.filter {
+//            it.trackName.lowercase().contains(request.lowercase()) ||
+//                    it.artistName.lowercase().contains(request.lowercase())
+//        }
+//        return result
+//    }
 }
